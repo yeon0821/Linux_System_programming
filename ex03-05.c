d14ee32e77c719d024372321a99fec2ef55af10e
yeon0821 <yeon8747@naver.com>
2023-03-24 12:30:46 +0900

    04

---
+++ ex03-05.c
@@ -0,0 +1,14 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+int main(int argc, char ** argv){
+	int filedes;
+	off_t newpos;
+
+	filedes = open(argv[1], O_RDONLY);
+
+	newpos = lseek(filedes, (off_t)0,SEEK_END);
+	
+	printf("file size : %ld\n", newpos);
+}
---
+++ filewr.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+void writeFile(void);
+void readFile(void);
+
+
+int main()
+{
+	
+	writeFile();
+	readFile();
+}
+
+void writeFile()
+{
+	FILE *fp;
+	fp = fopen("testFile", "W");
+	fprintf(fp, "File Write Test\n");
+	char testStr[1024] = "Test String\n";
+	fwrite(testStr, 100, 1, fp);
+	fclose(fp);
+}
+
+void readFile()
+{
+
+	FILE *fp;
+	char str[1024];
+	fgets(str, 1024, fp);	
+	printf("%s", str);
+	fread(str, 1024, 1, fp);
+	 printf("%s\n", str);
+	fclose(fp);
+}
